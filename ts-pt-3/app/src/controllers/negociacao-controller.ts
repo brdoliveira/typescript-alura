06fad220977ab66a7517c7b694b1544dfed9bb76
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-04 23:53:11 -0300

    FEAT(negociacao-controller): Criando método importaDados()

--- ts-pt-3/app/src/controllers/negociacao-controller.ts
+++ ts-pt-3/app/src/controllers/negociacao-controller.ts
@@ -2,8 +2,10 @@ import { domInjector } from '../decorators/dom-injector.js';
 import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { NegociacoesService } from '../services/negociacoes-service.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -19,6 +21,8 @@ export class NegociacaoController {
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
 
+    private negociacoesService = new NegociacoesService();
+
     constructor() {
         this.negociacoesView.update(this.negociacoes);
     }
@@ -26,9 +30,6 @@ export class NegociacaoController {
     @inspect
     @logarTempoDeExecucao()
     public adiciona(): void {
-        /*
-            Zé, você já viu isso?
-        */
         const negociacao = Negociacao.criaDe(
             this.inputData.value, 
             this.inputQuantidade.value,
@@ -44,6 +45,15 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+
+    public importaDados(): void {
+        this.negociacoesService.obterNegociacoesDoDia().then(negociacoesDeHoje => {
+            for(let negociacao of negociacoesDeHoje){
+                this.negociacoes.adiciona(negociacao)
+            }
+            this.negociacoesView.update(this.negociacoes)
+        })
     }
 
     private ehDiaUtil(data: Date) {
